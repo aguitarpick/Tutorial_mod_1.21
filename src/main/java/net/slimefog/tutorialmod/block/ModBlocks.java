eda895626e07c5888e7eff49c8edbe2faf81339a
SLIMEfog <woodcutter09@proton.me>
2026-01-26 12:27:10 -0500

    Finished Part 5

--- src/main/java/net/slimefog/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/slimefog/tutorialmod/block/ModBlocks.java
@@ -1,15 +1,15 @@
 package net.slimefog.tutorialmod.block;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.slimefog.tutorialmod.TutorialMod;
 
 public class ModBlocks {
@@ -18,6 +18,12 @@ public class ModBlocks {
     public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block", new Block(AbstractBlock.Settings.create().strength(3f)
             .requiresTool().sounds(BlockSoundGroup.WOOD)));
 
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(3f).requiresTool()));
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -31,11 +37,5 @@ public class ModBlocks {
 
     public static void registerModBlocks() {
         TutorialMod.LOGGER.info("Registering Mod Blocks for " + TutorialMod.MOD_ID);
-
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries -> {
-                fabricItemGroupEntries.add(ModBlocks.PINK_GARNET_BLOCK);
-                fabricItemGroupEntries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
-                });
-
     }
 }
